5254f42c094e22eed88ba6e4e59037d68e29f63e
Ivan <111454954+ffkeff@users.noreply.github.com>
2023-12-10 20:19:37 +0000

    Update client.c

--- client/client.c
+++ client/client.c
@@ -5,9 +5,15 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/wait.h>
+#include <assert.h>
+#include <string.h>
+
 
 #define HOST "127.0.0.1"
-#define PORT "2323"
+#define PORT "7777"
+
+char *secure_key = "1234353452";
 
 /*make socket by host and port*/
 int
@@ -20,7 +26,7 @@ make_socket(char *host, char *port){
 	}, *serv_info, *p;
 
 	int retval;
-	if((retval = getaddinfo(host, port, &hints, &serv_info)) != 0){
+	if((retval = getaddrinfo(host, port, &hints, &serv_info)) != 0){
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(retval));
 		exit(EXIT_FAILURE);
 	}
@@ -32,7 +38,7 @@ make_socket(char *host, char *port){
 			continue;
 		}
 
-		if(connect(sock, p->addr, p->addrlen) < 0){
+		if(connect(sock, p->ai_addr, p->ai_addrlen) < 0){
 			close(sock);
 			continue;
 		}
@@ -43,32 +49,48 @@ make_socket(char *host, char *port){
 }
 
 int
-main(){
+main(void){
 	
+	/*create connection with server*/
 	int cfd = make_socket(HOST, PORT);
-	fprintf(stdout, "success connect!\n");
 
+	/*create the pipe*/
 	int pipefd[2];
-	if(pipe(pipefd) < 0)
+	if(pipe(pipefd) < 0){
+		fprintf(stderr, "[Error] pipe()\n");
 		return EXIT_FAILURE;
-
-	pid_t pid;
-	if((pid = fork()) < 0)
-		return EXIT_FAILURE;
-
-	if(pid == 0){
-
-		close(pipefd[0]);
-
-		dup2(pipefd[1], stdout);
-
-		close(pipefd[1]);
-
-		char *args[] = {"", "6", NULL};
-		execve("", args, NULL);
-	} else {
-
 	}
+
+	/*create a child process*/
+	pid_t pid;
+	if((pid = fork()) < 0){
+		fprintf(stderr, "[Error] fork()\n");
+		return EXIT_FAILURE;
+	}
+
+	if (pid == 0) {
+
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+
+        char *args[] = {"../ecc", secure_key, NULL};
+        execve("../ecc", args, NULL);
+    } else {
+
+        close(pipefd[1]);
+        char buf;
+
+		/*send public key to server*/
+        while(read(pipefd[0], &buf, 1) > 0)
+            send(cfd, &buf, 1, 0);
+
+		/*read public key from server*/
+		while(recv(cfd, &buf, 1, 0) > 0)
+			write(STDOUT_FILENO, &buf, 1);
+
+        close(pipefd[0]);
+    }
 
 	return 0;
 }
